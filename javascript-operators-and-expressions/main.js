b61c0ac6397b8c9ffdea4bf3cade0e8a209c9bb3
Brenda Trieu <trieubrenda@gmail.com>
2023-01-30 20:05:06 +0000

    Completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+// i
+var width = 4;
+var height = 5;
+var area = width * height;
+console.log('Area: ', area);
+console.log('Type of area: ', typeof area);
+
+// ii
+var bill = 2;
+var payment = 10;
+var change = payment - bill;
+console.log('Change: ', change);
+console.log('Type of change: ', typeof change);
+
+// iii
+var quizzes = 5;
+var midterm = 10;
+var final = 15;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Grade: ', grade);
+console.log('Type of grade: ', typeof grade);
+
+// iv
+var firstName = 'Brenda';
+var lastName = 'Trieu';
+var fullName = firstName + ' ' + lastName;
+console.log('Full Name: ', fullName);
+console.log('Type of fullName: ', typeof fullName);
+
+// v
+var pH = 9;
+var isAcidic = pH < 7;
+console.log('Is Acidic: ', isAcidic);
+console.log('Type of isAcidic: ', typeof isAcidic);
+
+// vi
+var headCount = 50;
+var isSparta = (headCount === 300);
+console.log('Is Sparta: ', isSparta);
+console.log('Type of isSparta: ', typeof isSparta);
+
+// vii
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('Motto: ', motto);
+console.log('Type of motto: ', typeof motto);
