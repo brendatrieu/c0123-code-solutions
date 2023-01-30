0e699b064817a641fda9daea656d119aef621443
Brenda Trieu <trieubrenda@gmail.com>
2023-01-30 18:17:42 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'Brenda Trieu';
+var isCool = false;
+var totalPets = 1;
+var unknown = null;
+var never;
+
+console.log('Full Name: ', fullName);
+console.log('Is Cool: ', isCool);
+console.log('Total Pets: ', totalPets);
+console.log('Unknown: ', unknown);
+console.log('Never: ', never);
+console.log('Type of fullName: ', typeof fullName);
+console.log('Type of isCool: ', typeof isCool);
+console.log('Type of totalPets: ', typeof totalPets);
+console.log('Type of unknown: ', typeof unknown);
+console.log('Type of never: ', typeof never);
