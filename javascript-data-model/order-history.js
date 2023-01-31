59002638ca1fe6137e24696699cb7db1596f2369
Brenda Trieu <trieubrenda@gmail.com>
2023-01-31 17:59:26 +0000

    Completed javascript-data-model.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,68 @@
+var orderHistory = [
+  {
+    orderDate: 'August 4, 2020',
+    totalCost: 34,
+    recipientName: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: 'August 8, 2020',
+    itemDetails: [{
+      name: 'Javascript for impatient programmers',
+      seller: 'Rauschmayer, Dr. Axel',
+      returnDeadline: 'Sep 7, 2020',
+      subtotal: 31.55
+    }],
+    totalItems: 1,
+    deliveryNotes: null
+  },
+  {
+    orderDate: 'July 19, 2020',
+    totalCost: 44.53,
+    recipientName: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveryDate: 'July 20, 2020',
+    itemDetails: [{
+      name: 'The Timeless Way of Building',
+      seller: 'Alexander, Christopher',
+      returnDeadline: 'Aug 19, 2020',
+      subtotal: 41.33
+    }],
+    totalItems: 1,
+    deliveryNotes: 'Your package was delivered. It weas handed directly to a resident.'
+  },
+  {
+    orderDate: 'July 4, 2020',
+    totalCost: 17.22,
+    recipientName: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveryDate: 'July 7, 2020',
+    itemDetails: [{
+      name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+      seller: null,
+      returnDeadline: 'Aug 5, 2020',
+      subtotal: 15.98
+    }],
+    totalItems: 1,
+    deliveryNotes: 'Your package was delivered. It was handed directly to a resident.'
+  },
+  {
+    orderDate: 'July 3, 2020',
+    totalCost: 138.93,
+    recipientName: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveryDate: 'July 5, 2020',
+    itemDetails: [{
+      name: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      seller: null,
+      returnDeadline: 'Aug 4, 2020',
+      subtotal: 94.95
+    },
+    {
+      name: 'The Art of Sql',
+      seller: 'Faroult, Stephane',
+      returnDeadline: 'Aug 4, 2020',
+      subtotal: 33.99
+    }],
+    totalItems: 2,
+    deliveryNotes: null
+  }
+];
