e39b935322d14e19484f5de22d517aa72e3b9890
Brenda Trieu <trieubrenda@gmail.com>
2023-02-10 19:36:11 +0000

    Completed javascript-local-storage exercise.

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,14 @@
+/* exported todos */
+
+var todos = [];
+
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+
+window.addEventListener('beforeunload', event => {
+  event.preventDefault();
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+});
