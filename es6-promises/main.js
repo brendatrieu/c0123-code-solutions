ae423051ff5d2aadaa0255f7c6efc052bb7cc644
Brenda Trieu <trieubrenda@gmail.com>
2023-03-15 21:24:51 +0000

    Updated to use 'catch' rather than 2 arguments in 'then'.

--- es6-promises/main.js
+++ es6-promises/main.js
@@ -3,7 +3,5 @@ import takeAChance from './take-a-chance.js';
 const chanceTaken = takeAChance('Brenda');
 
 chanceTaken
-  .then(
-    (value) => console.log(value),
-    (error) => console.error(error.message)
-  );
+  .then((value) => console.log(value))
+  .catch((error) => console.error(error.message));
