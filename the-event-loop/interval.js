bc358e511ed75d250b8f5ccb0c791fa6b927ea48
Brenda Trieu <trieubrenda@gmail.com>
2023-03-15 17:49:05 +0000

    Completed the-event-loop exercise.

---
+++ the-event-loop/interval.js
@@ -0,0 +1,18 @@
+// Set up initial number to count down from
+let currCount = 3;
+
+// Create a function to countdown to 0
+function countDown() {
+  if (currCount) {
+    console.log(currCount);
+    return currCount--;
+  } else {
+    console.log('Blast off!');
+    clearInterval(countId);
+  }
+}
+
+// Initiate blast off
+const countId = setInterval(() => {
+  countDown(currCount);
+}, 1000);
---
+++ the-event-loop/timeout.js
@@ -0,0 +1,7 @@
+// Initial message to the user
+console.log('Hello, just a moment...');
+
+// Secondary message after 2 seconds
+setTimeout(() => {
+  console.log('Thanks for waiting!');
+}, 2000);
