1096c0255f692bc44ab398b943ef8434bd9709ca
Brenda Trieu <trieubrenda@gmail.com>
2023-03-15 21:17:31 +0000

    Completed es6-promises exercise.

--- es6-promises/main.js
+++ es6-promises/main.js
@@ -3,5 +3,7 @@ import takeAChance from './take-a-chance.js';
 const chanceTaken = takeAChance('Brenda');
 
 chanceTaken
-  .then((value) => console.log(value))
-  .catch((reason) => console.error(`${reason}`));
+  .then(
+    (value) => console.log(value),
+    (error) => console.error(error.message)
+  );
