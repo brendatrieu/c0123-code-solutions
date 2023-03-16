eab52c5c6802d209cf5fd02205778620498c326e
Brenda Trieu <trieubrenda@gmail.com>
2023-03-16 19:13:25 +0000

    Modified error handling to show invalid path, if any.

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -3,8 +3,8 @@ import { readFile } from 'node:fs/promises';
 function readAll() {
   const [, , ...filePaths] = process.argv;
   filePaths.forEach((path) => {
-    if (path.endsWith('.txt')) {
-      return;
+    if (!path.endsWith('.txt')) {
+      console.error('invalid path:', path);
     }
     readFile(path, 'utf8')
       .then((contents) => console.log(contents))
