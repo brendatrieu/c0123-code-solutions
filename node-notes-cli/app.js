a27f14594c1df2a33b05aca1ccac20e438628320
Brenda Trieu <trieubrenda@gmail.com>
2023-03-17 17:02:12 +0000

    Updated with feedback for improved readability and proper async processing.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,64 +1,61 @@
 import { writeFile } from 'node:fs/promises';
 import { readFileSync } from 'node:fs';
 
-// Set up function and variables to handle user inputs
+// ------- Set up function and variables to handle user inputs -------
+const allInputs = process.argv;
 const action = process.argv[2];
 const arg3 = process.argv[3];
 const arg4 = process.argv[4];
 const legalActions = ['read', 'create', 'update', 'delete'];
 
-function interpretInputs() {
-  switch (action) {
-    case 'read':
-      readNotes();
-      break;
-    case 'create':
-      newNote();
-      break;
-    case 'update':
-      updateNote();
-      break;
-    case 'delete':
-      deleteNote();
-      break;
-    default:
-      console.error(action, 'is not an acceptable action. Please try again with:', legalActions);
-      process.exit(1);
+async function interpretInputs() {
+  try {
+    switch (action) {
+      case 'read':
+        await readNotes();
+        break;
+      case 'create':
+        await newNote(arg3);
+        break;
+      case 'update':
+        await updateNote(arg3, arg4);
+        break;
+      case 'delete':
+        await deleteNote(arg3);
+        break;
+      default:
+        console.error(action, 'is not an acceptable action. Please try again with:', legalActions);
+        process.exit(1);
+    }
+  } catch (err) {
+    handleError(err);
   }
 }
 
-/**
- * Reusable functions
- */
+// ------- Reusable functions -------
 
-// Catch error handling
 function handleError(err) {
   console.error(err);
   process.exit(1);
 }
 
-// Evaluate ID
-async function evaluateId() {
-  try {
-    const notes = await getJournal('notes');
-    const validIds = Object.keys(notes);
+/** Review the ID of interest and see whether it numerical and an ID that exists within the working database, then exit the process */
+function evaluateId(id, notes) {
+  const validIds = Object.keys(notes);
 
-    if (typeof Number(arg3) !== 'number' || !validIds.includes(arg3)) {
-      console.error(arg3, 'is not a valid ID. Please provide a valid ID:', validIds.join(', '));
-      process.exit(1);
-    }
-  } catch (err) {
-    handleError();
+  if (typeof Number(id) !== 'number' || !validIds.includes(id)) {
+    console.error(id, 'is not a valid ID. Please provide a valid ID:', validIds.join(', '));
+    process.exit(1);
   }
 }
 
-// Multiple notes
+/** Prints a message to notify the user that they inputted too many arguments, then exit the process */
 function tooManyNotes() {
   console.error('Please note only one note at a time can be inputted. Try again and ensure your note is enclosed in quotation marks.');
   process.exit(1);
 }
 
-// Journal retrieval
+/** Read the data file and parse it into a usable form. If the user passes in a desired item, then return that part of the object only */
 async function getJournal(item) {
   try {
     const journal = await readFileSync('data.json', 'utf8');
@@ -72,18 +69,17 @@ async function getJournal(item) {
   }
 }
 
-// Journal update
+/** Take the input, stringify it and write it over the data file */
 async function writeJournal(input) {
   try {
-    await writeFile('data.json', input);
+    const newJournal = JSON.stringify(input);
+    await writeFile('data.json', newJournal);
   } catch (err) {
     handleError(err);
   }
 }
 
-/**
- * Action functions
- */
+// ------- Action functions -------
 
 async function readNotes() {
   try {
@@ -97,61 +93,58 @@ async function readNotes() {
   }
 }
 
-async function newNote() {
+async function newNote(note) {
   try {
-    if (process.argv.length > 4) {
+    if (allInputs.length > 4) {
       tooManyNotes();
     }
     const journal = await getJournal();
     const nextId = await getJournal('nextId');
-    let newJournal = null;
 
-    journal.notes[nextId] = arg3;
+    journal.notes[nextId] = note;
     journal.nextId = nextId + 1;
-    newJournal = JSON.stringify(journal);
-    writeJournal(newJournal);
+    await writeJournal(journal);
   } catch (err) {
     handleError(err);
   }
 }
 
-async function updateNote() {
+async function updateNote(id, note) {
   try {
-    await evaluateId();
+    const noteEntries = await getJournal('notes');
+    evaluateId(id, noteEntries);
 
-    if (process.argv.length > 5) {
+    if (allInputs.length > 5) {
       tooManyNotes();
     }
 
     const journal = await getJournal();
-    let newJournal = null;
 
-    journal.notes[arg3] = arg4;
-    newJournal = JSON.stringify(journal);
-    writeJournal(newJournal);
+    journal.notes[id] = note;
+    await writeJournal(journal);
   } catch (err) {
     handleError(err);
   }
 }
 
-async function deleteNote() {
+async function deleteNote(id) {
   try {
-    await evaluateId();
+    const noteEntries = await getJournal('notes');
+    evaluateId(id, noteEntries);
 
-    const journal = await getJournal();
-    let newJournal = null;
-
-    if (process.argv.length > 4) {
-      const remainder = process.argv.splice(4);
-      console.log('Be advised, the following was not processed:', remainder.join(' '));
+    if (allInputs.length > 4) {
+      const remainder = allInputs.splice(4);
+      console.error('Be advised, the following was not processed:', remainder.join(' '), 'Please try delete action again without notes.');
+      process.exit(1);
     }
 
-    delete journal.notes[arg3];
-    newJournal = JSON.stringify(journal);
-    writeJournal(newJournal);
+    const journal = await getJournal();
+
+    delete journal.notes[id];
+    await writeJournal(journal);
   } catch (err) {
     handleError(err);
   }
 }
 
-interpretInputs();
+await interpretInputs();
