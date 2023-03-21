7598767d73dcbca42b920e7f8eff8dbad24d4d76
Brenda Trieu <trieubrenda@gmail.com>
2023-03-21 18:27:17 +0000

    Updated per feedback.

--- express-post-json/server.js
+++ express-post-json/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.get('/api/grades', (req, res) => res.json(Object.values(grades)));
 
 app.post('/api/grades', (req, res) => {
-  grades[nextId] = { id: nextId, ...req.body };
+  grades[nextId] = { ...req.body, id: nextId };
   res.status(201).send(grades[nextId]);
   return nextId++;
 });
