59de8cc07489d71482cbb498c1139d5ef721cba7
Brenda Trieu <trieubrenda@gmail.com>
2023-03-29 19:20:26 +0000

    Completed react-toggle-switch-component exercise.

--- react-toggle-switch-component/my-app/src/ToggleSwitch.js
+++ react-toggle-switch-component/my-app/src/ToggleSwitch.js
@@ -1,7 +1,29 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import './ToggleSwitch.css';
 
-// export default function ToggleSwitch() {
-//   const [switch, setSwitch] = useState(false);
+export default function ToggleSwitch() {
+  const [isClicked, setIsClicked] = useState(false);
+  const on = {
+    backgroundColor: "#69A84F",
+    justifyContent: "flex-end"
+  };
+  const off = {
+    backgroundColor: "#E2E2E2",
+    justifyContent: "flex-start"
+  };
+  const styleState = isClicked ? on : off;
 
+  return (
+    <>
+      <div className="switch-div" style={styleState}>
+        <button className="switch-button"
+          onClick={() => setIsClicked(!isClicked)}>
+          </button>
+      </div>
+      <p className="switch-label">
+        {isClicked ? "ON" : "OFF"}
+      </p>
+    </>
+  )
 
-// }
+}
