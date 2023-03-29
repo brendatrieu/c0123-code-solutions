9c5e7d371864cc48328c99926d54f43eaec04396
Brenda Trieu <trieubrenda@gmail.com>
2023-03-29 21:58:52 +0000

    Completed react-form-controls exercise.

--- react-form-controls/my-app/src/RegistrationFormControlled.js
+++ react-form-controls/my-app/src/RegistrationFormControlled.js
@@ -1,21 +1,21 @@
 import { useState } from 'react';
 
 export default function RegistrationFormUncontrolled() {
-  const [un, setUn] = useState("");
-  const [pw, setPw] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log({un, pw})
+    console.log({ username, password })
   }
   return (
     <>
       <form id="controlled-form">
         <label>
-          Username: <input name="username" type="text" value={un} onChange={(e) => setUn(e.target.value)}/>
+          Username: <input name="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
         </label>
         <label>
-          Password: <input name="password" type="password" value={pw} onChange={(e) => setPw(e.target.value)} />
+          Password: <input name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </label>
         <button type="submit" onClick={handleSubmit}>Submit</button>
       </form>
