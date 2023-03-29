7c2c7b8fb7d8bf913fd58b5646c2e7ca01c00da0
Brenda Trieu <trieubrenda@gmail.com>
2023-03-29 17:52:37 +0000

    Updated per feedback.

--- react-hot-button-component/my-app/src/HotButton.js
+++ react-hot-button-component/my-app/src/HotButton.js
@@ -2,12 +2,12 @@ import { useState } from 'react';
 
 export default function HotButton () {
   const allColors = ['#000000', '#351C74', '#674DA7', '#E06666', '#F6B26A', '#FEFF00', '#FFFFFF'];
-  let [numClicks, setNumClicks] = useState(0);
-  const bgColor = (numClicks % 3 === 0) ? allColors[numClicks/3] : allColors[Math.floor(numClicks / 3)];
-  let fontColor = (allColors.indexOf(bgColor) < 4) ? "white" : "black";
+  const [numClicks, setNumClicks] = useState(0);
+  const bgColor = allColors[Math.floor(numClicks / 3)];
+  const fontColor = (allColors.indexOf(bgColor) < 4) ? "white" : "black";
   return (
     <button style={{color: fontColor, backgroundColor: bgColor}}
-      onClick={() => setNumClicks(numClicks !== 20 ? numClicks + 1 : numClicks = 0)} >
+      onClick={() => setNumClicks(numClicks !== 20 ? numClicks + 1 : 0)} >
         Hot Button
     </button>
   )
