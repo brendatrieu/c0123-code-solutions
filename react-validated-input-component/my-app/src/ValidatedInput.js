745be44dd8cc907df26b7f9d3828ea9190946eed
Brenda Trieu <trieubrenda@gmail.com>
2023-03-30 17:15:47 +0000

    Updated formatting and incorporated feedback.

--- react-validated-input-component/my-app/src/ValidatedInput.js
+++ react-validated-input-component/my-app/src/ValidatedInput.js
@@ -3,27 +3,25 @@ import { useState } from 'react';
 export default function ValidatedInput() {
   const [pw, setPw] = useState('');
   const [isValid, setIsValid] = useState(false);
-  const [attempted, setAttempted] = useState(false);
   const [err, setErr] = useState([]);
 
   function validate(e) {
     const allReqs = /(?=.*[!@#$%^&*()])(?=.*\d)(?=.*[A-Z]).*/;
     const typedPw = e.target.value;
     const check = allReqs.test(typedPw) && (typedPw.length > 7);
-
+    let message = [];
     setIsValid(check);
     setPw(typedPw);
-    setAttempted(true);
 
     if (!check) {
-      const msgStyle = { margin: "10px", color: "red" };
-      if (attempted && typedPw.length === 0) {
-        return setErr(<p style={msgStyle}>Password is a required field.</p>);
+      // --- Password Required ---
+      const reqPw = 'Password is a required field.';
+      if (typedPw.length === 0) {
+        message.push(reqPw);
+        return setErr(message);
       }
-      let message = [];
       // --- Min 8 Characters ---
       const noEightChar = 'Password must be at least 8 characters long.';
-      // debugger;
       (typedPw.length < 8) ? message.push(noEightChar) : message.filter((msg) => msg !== noEightChar);
       // --- Special Characters ---
       const specChar = /[!@#$%^&*()]/;
@@ -37,25 +35,24 @@ export default function ValidatedInput() {
       const upper = /[A-Z]/;
       const noUpper = 'Password must include at least one uppercase letter.';
       !upper.test(typedPw) ? message.push(noUpper) : message.filter((msg) => msg !== noUpper);
-      setErr(message.map((errorMessage) => <p key={errorMessage} style={msgStyle}>{errorMessage}</p>));
+      return setErr(message);
     }
+    return setErr(message);
   }
   const invalid = "fa-sharp fa-solid fa-xmark";
   const valid = "fa-sharp fa-solid fa-check";
+  const msgStyle = { margin: "10px", color: "red" };
 
   return (
     <>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
       <form style={{ margin: "10px" }}>
         <label htmlFor="password" style={{display: "block"}}>
           Password
         </label>
         <input name="password" type="password" value={pw} onChange={(e) => validate(e)} />
-        { attempted && <i style={{ margin: "10px", color: isValid ? "green" : "red" }} className={isValid ? valid : invalid} />}
+        { (err.length > 0 || isValid) && <i style={{ margin: "10px", color: isValid ? "green" : "red" }} className={isValid ? valid : invalid} />}
       </form>
-      {err}
+      {err.map((errorMessage) => <p key={errorMessage} style={msgStyle}>{errorMessage}</p>)}
     </>
   )
 }
