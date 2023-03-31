dda45c0363dfb02bafe1dcc4861f0008fc56a773
Brenda Trieu <trieubrenda@gmail.com>
2023-03-31 18:41:40 +0000

    Reorganized component responsibilities.

--- react-app-drawer-component/my-app/src/App.js
+++ react-app-drawer-component/my-app/src/App.js
@@ -6,31 +6,18 @@ const items = ['About', 'Get Started', 'Sign In'];
 
 function App() {
   const [title, setTitle] = useState('');
-  const [view, setView] = useState('main');
-  const overlay = (view === 'main') ?
-    { backgroundColor: 'rgba(255, 255, 255, 0)', pointerEvents: 'none' } :
-    { backgroundColor:'rgba(255, 255, 255, 0.25)', pointerEvents:'all' };
-
-  function toggleMenu() {
-    setView(view === 'main' ? '' : 'main');
-  }
 
   return (
     <div>
-      <AppDrawer view={view}
+      <AppDrawer
         items={items}
         heading="Menu"
         onTitleChange={(item) => setTitle(item)}
-        onMenuToggle={toggleMenu} />
-      <div style={overlay}
-        className="overlay"
-        onClick={() => view!=='main' && toggleMenu()}>
-      </div>
+      />
       <header className="App-header">
         {title}
         <a href="https://www.google.com" target="_blank" rel="noreferrer">Google</a>
       </header>
-
     </div>
   );
 }
--- react-app-drawer-component/my-app/src/AppDrawer.js
+++ react-app-drawer-component/my-app/src/AppDrawer.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import { FaHamburger } from 'react-icons/fa';
 import './AppDrawer.css';
 
@@ -9,31 +9,44 @@ import './AppDrawer.css';
  * @param {items} An array containing pages listed within the menu.
  */
 
-export default function AppDrawer({heading, items, onTitleChange, onMenuToggle, view}) {
+export default function AppDrawer({heading, items, onTitleChange}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const overlay = (!isOpen) ?
+    { backgroundColor: 'rgba(255, 255, 255, 0)', pointerEvents: 'none' } :
+    { backgroundColor: 'rgba(255, 255, 255, 0.25)', pointerEvents: 'all' };
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
 
   return (
-    <div style={{position: 'absolute'}}>
-      {(view === 'main') && <FaHamburger className="hamburger-icon" onClick={onMenuToggle}/>}
-      <DrawerItems heading={heading}
-        items={items}
-        view={view}
-        onMenuToggle={onMenuToggle}
-        onTitleChange={onTitleChange}
-      />
-    </div>
+    <>
+      <div style={{position: 'absolute'}}>
+        {(!isOpen) && <FaHamburger className="hamburger-icon" onClick={toggleMenu}/>}
+        <DrawerItems heading={heading}
+          items={items}
+          isOpen={isOpen}
+          onMenuToggle={toggleMenu}
+          onTitleChange={onTitleChange}
+        />
+      </div>
+      <div style={overlay}
+          className="overlay"
+          onClick={() => isOpen && toggleMenu()}>
+      </div>
+    </>
   )
 }
 
-function DrawerItems({heading, items, view, onMenuToggle, onTitleChange}) {
+function DrawerItems({heading, items, isOpen, onMenuToggle, onTitleChange}) {
   function handleClick(item) {
     onTitleChange(item);
     onMenuToggle();
   }
   return (
-    <div className={view !== 'main' ? 'menu' : 'hidden'}>
+    <div className={isOpen ? 'menu' : 'hidden'}>
       <h2>{heading}</h2>
       <ul className="menu-list">
-        {items.map((item) => <li className="menu-item" onClick={() => handleClick(item)}>{item}</li>)}
+        {items.map((item) => <li key={item} className="menu-item" onClick={() => handleClick(item)}>{item}</li>)}
       </ul>
     </div>
   )
