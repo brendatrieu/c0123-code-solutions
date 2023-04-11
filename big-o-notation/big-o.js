38ecb7a45b3f1446850eb777eda4cc101ee8e4a2
Brenda Trieu <trieubrenda@gmail.com>
2023-04-11 20:39:50 +0000

    Edited coefficients.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -11,7 +11,7 @@ function uniqueLinear(words) {
     const word = words[i];          // 2 * n = O(2n) ~= O(n)
     if (!seen[word]) {              // 2 * n = O(2n) ~= O(n)
       seen[word] = true;            // 2 * n = O(2n) ~= O(n)
-      unique[unique.length] = word; // 3 * n = O(3n) ~= O(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~= O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
@@ -28,7 +28,7 @@ function uniqueQuadratic(words) {
     let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n)
-      c < i;                        // 2 * n * n = O(2n^2) ~= O(n^2)
+      c < i;                        // 1 * n * n = O(2n^2) ~= O(n^2)
       c++                           // 2 * n * n = O(2n^2) ~= O(n^2)
     ) {
       const comparing = words[c];   // 2 * n * n = O(2n^2) ~= O(n^2)
@@ -36,8 +36,8 @@ function uniqueQuadratic(words) {
         isUnique = false;           // 1 * n * n = O(n^2)
       }
     }
-    if (isUnique) {                 // 2 * n = O(2n) ~= O(n)
-      unique[unique.length] = word; // 3 * n * n= O(3n^2) ~= O(n^2)
+    if (isUnique) {                 // 1 * n = O(2n) ~= O(n)
+      unique[unique.length] = word; // 2 * n * n = O(3n^2) ~= O(n^2)
     }
   }
   return unique;                    // 1 * 1 = O(1)
