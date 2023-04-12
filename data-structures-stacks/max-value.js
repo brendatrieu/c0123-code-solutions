7373dc27a81c609ed0066e934ea3bb8f00d63867
Brenda Trieu <trieubrenda@gmail.com>
2023-04-12 17:46:49 +0000

    Minor update to max value function.

--- data-structures-stacks/max-value.js
+++ data-structures-stacks/max-value.js
@@ -2,7 +2,7 @@ export default function maxValue(stack) {
   let maxValue = -Infinity;
   while (stack.peek() !== undefined) {
     const top = stack.pop();
-    (maxValue < top) && (maxValue = top);
+    if (maxValue < top) (maxValue = top);
   }
   return maxValue;
 }
